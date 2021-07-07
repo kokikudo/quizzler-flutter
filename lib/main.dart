d65af157c34c023141dc979af67c24b25b564fb8
kokikudo <massanmesu@icloud.com>
2021-07-07 13:26:29 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+//TODO 2 import package
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,58 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> _scoreKeeper = [];
+
+  Icon _correctIcon = Icon(
+    Icons.check,
+    color: Colors.green,
+  );
+  Icon _wrongIcon = Icon(
+    Icons.close,
+    color: Colors.red,
+  );
+
+  void _judge(bool pickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      if (pickedAnswer == correctAnswer) {
+        setState(() {
+          _scoreKeeper.add(_correctIcon);
+        });
+      } else {
+        setState(() {
+          _scoreKeeper.add(_wrongIcon);
+        });
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+
+  //TODO 3 create Function that show Alert
+  void _onBasicAlertPressed(context) async {
+    if (_scoreKeeper.length == quizBrain.getQuestionLength()) {
+      var _correctCount =
+          _scoreKeeper.where((icon) => icon == _correctIcon).length;
+      final _quizCount = _scoreKeeper.length;
+
+      await Alert(
+        context: context,
+        title: 'Finished!',
+        desc: 'Your score is $_correctCount / $_quizCount',
+      ).show();
+
+      _resetQuestion();
+    }
+  }
+
+  void _resetQuestion() {
+    setState(() {
+      quizBrain.resetQuestionNumber();
+      _scoreKeeper.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,61 +92,54 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
+                quizBrain.getQuestionText(),
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
             ),
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
+            child: Container(
+          margin: EdgeInsets.all(15),
+          color: Colors.green,
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                _judge(true);
+                //TODO 4 callback Alert Function
+                _onBasicAlertPressed(context);
+              });
+            },
+            child: Text(
+              'True',
+              style: TextStyle(
+                color: Colors.white,
               ),
-              onPressed: () {
-                //The user picked true.
-              },
             ),
           ),
-        ),
+        )),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
+            child: Container(
+          margin: EdgeInsets.all(15),
+          color: Colors.red,
+          child: TextButton(
+            onPressed: () {
+              _judge(false);
+              //TODO 5 copy TODO 4
+              _onBasicAlertPressed(context);
+            },
+            child: Text(
+              'False',
+              style: TextStyle(
+                color: Colors.white,
               ),
-              onPressed: () {
-                //The user picked false.
-              },
             ),
           ),
+        )),
+        Row(
+          children: _scoreKeeper,
         ),
-        //TODO: Add a Row here as your score keeper
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
